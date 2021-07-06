274a74de88ce1e515a8ab1d6d171c44b18061728
Hung-Wei Cheng <maxchengboy@gmail.com>
2021-07-06 23:09:24 +0800

    wrong

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use std::io;
 
-fn main() {
-    println!("insert a word");
-
-    let mut guess = String::new(); //you can change the value
-    //let guess2 = String::new(); // unchangeable
-
-    // mut => mutable(changeable)
-    // :: => associated
-    // => let creat a valuable and we bound it to a new empty String
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    // io::stdin is equal to std::io::stdio if we didn't use std::io at first
-    // then it reads the value and append it to the variable instead of overwrite
-    // most importantly it returns the value using io::Result
-    // & is reference which is the same as C
-    // expect is the part that handles the error
-
-    // without expect the code will be compiled with errors
-
-    println!("You typed: {}", guess);
-    // something like printf
+fn main(){
+    let x = 5;
+    println!("x is set to {}",x);
+    x = 6;
+    println!("x is changed to {}",x);
+    //wrong
 }
