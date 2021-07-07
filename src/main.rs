6775e3a231ae4ed0b3c749759f3e7967059f945e
Hung-Wei Cheng <maxchengboy@gmail.com>
2021-07-07 14:59:21 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,39 @@
-//use std::io;
+use std::io;
 
-fn main() {
-    let mut x = 1;
-    // we need mut to let variable mutable
-    println!("x is set to {}", x);
-    x = 6;
-    println!("x is changed to {}", x);
-    x = x * 6;
-    println!("x is changed to {}", x);
-    x = x / 7;
-    println!("x is changed to {}", x);
+fn main(){
+    println!("please input a num!");
 
-    // in rust variables are a little different from c
-    // rust force all variable set to constant (immutable)
-    // if we need value to be changed we need to add "mut" when declared
+    // Array & Tuple
+    /*let arr = [93_22_2,0xff,0o77];
+    // underline means nothing. it is just like a common in and writing. For the better understanding
+    // let tuple: (i32,i32,i32,u8) = (93_22_2,0xff,0o77,b'A');
+    // simple way
+    let tuple = (93_22_2,0xff,0o77,b'A');
+    let (a,b,c,d) = tuple;
+    //println!("{}",tuple); not permitted
+    println!("{}、{}、{}、{}",a,b,c,d);
+    println!("{}、{}、{}、{}",tuple.0,tuple.1,tuple.2,tuple.3);*/
 
-    let foo = "empty";
-    println!("foo is set to '{}'",foo);
-    let foo = foo.len();
-    println!("foo is {}", foo);
-    let foo = "I am confused";
-    println!("foo is now '{}'",foo);
-    // foo is first set to a string
-    // then another foo is set to a number
-    // then foo is set to a string again
-    // Why, it just change the whole thing?
-    // This process is called shadowing
-    // the new foo will be replaced by another new foo
-    // because rust thinks they are different
-    // though simply declare a variable can' t be modified
-    // you can just declare another one with the same name and replace it
+    // formatted array
+    /*let months:[&str;12] = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    for month in months{
+        println!("{}",month);
+    }*/
+    /*let mut number=String::new();
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read the line");
+    let number: isize = number
+        .trim()
+        .parse()
+        .expect("index entered was not a number");
+    println!("the number you entered is {}",number);*/
 
-    const FOO:&str = "constant";
-    println!("FOO is {}",FOO);
-    // const FOO:&str = "another";
-    // The correct way for constant, you can never overwrite it
-    // constant names in rust is UPPER_CASE in convention
-    let mut foo2 = "empty";
-    println!("foo2 is {}",foo2);
-    foo2 = "things";
-    println!("foo2 is {}",foo2);
-    //fOO = 100;
-    // you can not change a str to num
-    //println!("fOO is {}",fOO);
-
+    let mut string = String::new();
+    string += "                          Love            ";
+    string = string
+        .trim().parse()
+        .expect("index transfer failed");
+    println!("string value is {}",string);
 }
